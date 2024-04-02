09419c7b578f0f768376f049070aa0fb44e1c841
naufaalkh <naufaalkh@gmail.com>
2024-04-02 16:41:58 +0700

    Input stuktur data

--- struckturC.cpp
+++ struckturC.cpp
@@ -8,3 +8,16 @@ struct Mahasiswa
     string alamat;
     int umur;
 };
+
+int main()
+{
+    Mahasiswa mhs; // Input stuktur data
+    cout << " Nomor Mahasiswa : ";
+    cin >> mhs.nim;
+    cout << " Nama Mahasiswa : ";
+    cin >> mhs.nama;
+    cout << " Alamat Mahasiswa : ";
+    cin >> mhs.alamat;
+    cout << " Umur Mahasiswa : ";
+    cin >> mhs.umur;
+}
