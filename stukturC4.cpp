b9e7bc2a8b4d7a9331522555ce5b0c21548996e2
naufaalkh <naufaalkh@gmail.com>
2024-04-02 16:54:32 +0700

    membuat struktur Mahasiswa

--- stukturC4.cpp
+++ stukturC4.cpp
@@ -1,3 +1,17 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+struct DetailAlamat
+{ // Membuat stuktur variabel alamat
+    string desa;
+    string kota;
+};
+
+struct Mahasiswa
+{ // Membuat stuktur variabel mahasiswa
+    string nim;
+    string nama;
+    DetailAlamat alamat;
+    int umur;
+};
