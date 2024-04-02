30fba2cbd7d98003803f6c7f46a4834431ec7c84
naufaalkh <naufaalkh@gmail.com>
2024-04-02 16:58:37 +0700

    membuat output

--- stukturC4.cpp
+++ stukturC4.cpp
@@ -36,4 +36,17 @@ int main()
         cin.ignore(1, '\n');
         cout << endl;
     }
+
+    for (int i = 0; i < 3; i++)
+    {
+        cout << endl;
+        cout << "Data Mahasiswa ke-" << (i + 1) << ":" << endl;
+        cout << "\nNIM : " << mhs[i].nim;
+        cout << "\nNama : " << mhs[i].nama;
+        cout << "\nAlamat : ";
+        cout << "\n\tDesa : " << mhs[i].alamat.desa;
+        cout << "\n\tKota : " << mhs[i].alamat.kota;
+        cout << "\nUmur : " << mhs[i].umur;
+        cout << endl;
+    }
 };
